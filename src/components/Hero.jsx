00d0b99a797a86acb3bcae066f92b545d784f284
ShreyVDesai <shreyvdesai@gmail.com>
2024-12-25 11:35:44 -0500

    Completed layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className = "w-full lg:w-1/2">
             <div className='flex flex-col items-center lg:items-start'>
                 <h1 className='pb-16 font-thin text-6xl tracking-tight lg:mt-16 lg:text-8xl'>Shrey Desai</h1>
-                <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">AI Engineer</span>
+                <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">AI Engineer</span>
                 <p className='my-2 tracking-tighter max-w-xl py-6 font-light'>{HERO_CONTENT}</p>
             </div>
         </div>
